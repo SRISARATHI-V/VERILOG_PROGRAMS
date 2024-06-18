e0a02ac4eca64153566a2f9dafd6b557811db9ff
SRISARATHI V <158599725+SRISARATHI-V@users.noreply.github.com>
2024-06-18 11:01:54 +0530

    Create Shiftreg_right.v

---
+++ Sequential circuits/day-2/Shiftreg_right.v
@@ -0,0 +1,13 @@
+module right_sr(clk,rst,din,dout);
+  input clk,rst,din;
+  output reg [3:0] dout;
+  always @ (posedge clk or posedge rst)
+    begin 
+      if (rst)
+        begin
+          dout <=4'b0000;
+        end
+      else 
+        dout={din,dout[2:0]};
+    end
+endmodule
