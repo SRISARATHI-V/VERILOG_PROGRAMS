50e4342631048dea50ec651ccac48ec025c2c3c5
SRISARATHI V <158599725+SRISARATHI-V@users.noreply.github.com>
2024-06-18 12:01:56 +0530

    Update lfsr_tb.v

--- Sequential circuits/day-2/lfsr_tb.v
+++ Sequential circuits/day-2/lfsr_tb.v
@@ -1,11 +1,15 @@
-module lfsr(clk,rst, op);
-  input clk,rst;
-  output reg [3:0]op;
-  always @(posedge clk)
-    begin
-      if(rst)
-        op=4'hf;
-      else
-        op= {op[2:0],(op[3]^op[2])};
-    end
+module lfsr_tb;
+  reg clk,rst;
+  wire [3:0]op;
+  lfsr lfsr_tb(clk,rst,op);
+  always #3 clk=~clk;
+  initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars(1);
+    
+    clk=0;
+    rst=1;#5
+    rst=0;#20
+    $finish;
+  end
 endmodule
